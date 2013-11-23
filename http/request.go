4cd65cfee7b5c82a62a7ef7a2c755db6a27070a7
Andy <andy@away.name>
2013-11-23 13:26:24 +0400

    remove buffer in write mode

--- http/request.go
+++ http/request.go
@@ -2,7 +2,6 @@ package http
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"io"
 	"net/url"
@@ -24,49 +23,39 @@ type Request struct {
 }
 
 func (req *Request) Write(w io.Writer) error {
-
-	bw := &bytes.Buffer{}
-
-	fmt.Fprintf(bw, "%s %s HTTP/1.1\r\n", valueOrDefault(req.Method, "GET"), req.URL.RequestURI())
-	fmt.Fprintf(bw, "Host: %s\r\n", req.Host)
-
-	userAgent := ""
-	if req.Header != nil {
-		if ua := req.Header["User-Agent"]; len(ua) > 0 {
-			userAgent = ua[0]
-		}
-	}
-	if userAgent != "" {
-		fmt.Fprintf(bw, "User-Agent: %s\r\n", userAgent)
-	}
-
+	headers := "Host: " + req.Host + "\r\n"
 	if req.Method == "POST" || req.Method == "PUT" {
-		fmt.Fprintf(bw, "Content-Length: %d\r\n", req.ContentLength)
+		headers += fmt.Sprintf("Content-Length: %d\r\n", req.ContentLength)
 	}
-
 	if req.Header != nil {
 		for key, values := range req.Header {
-			if key == "User-Agent" || key == "Content-Length" || key == "Host" {
+			if key == "Content-Length" || key == "Host" {
 				continue
 			}
 			for _, value := range values {
-				fmt.Fprintf(bw, "%s: %s\r\n", key, value)
+				headers += key + ": " + value + "\r\n"
 			}
 		}
 	}
-
-	io.WriteString(bw, "\r\n")
-
+	pocket := fmt.Sprintf("%s %s HTTP/1.1\r\n%s\r\n",
+		valueOrDefault(req.Method, "GET"),
+		req.URL.RequestURI(),
+		headers,
+	)
+	_, err := io.WriteString(w, pocket)
+	if err != nil {
+		return err
+	}
 	if req.Method == "POST" || req.Method == "PUT" {
+		req.BufferSize = req.ContentLength
 		bodyReader := bufio.NewReader(req.Body)
-		_, err := bodyReader.WriteTo(bw)
+		_, err := bodyReader.WriteTo(w)
 		if err != nil {
 			return err
 		}
 	}
-	req.BufferSize = int64(bw.Len())
-	_, err := bw.WriteTo(w)
-	return err
+	req.BufferSize += int64(len(pocket))
+	return nil
 }
 
 func valueOrDefault(value string, def string) string {
